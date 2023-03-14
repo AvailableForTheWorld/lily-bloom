b43566ea711955f2ff9376fd11a5f8719e33084d
AvailableForTheWorld <435829792@qq.com>
2023-03-14 23:42:23 +0800

    feat: three类型添加

--- src/views/day1/index.vue
+++ src/views/day1/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>666</div>
+  <canvas id="c"></canvas>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { WebGLRenderer } from 'three'
+const mainFunc = () => {
+  const canvas = document.getElementById('c') as HTMLCanvasElement
+  // create renderer
+  const renderer = new WebGLRenderer({ antialias: true, canvas })
+}
+</script>
 
 <style lang="scss" scoped></style>
